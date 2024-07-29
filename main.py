cd660d923e3d5c867d9afe48534495cca569824f
Abdessamad Enabih <70137147+AbdessamadEnabih@users.noreply.github.com>
2024-07-29 13:44:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 from fastapi.responses import JSONResponse
-
+from .models import (
+    Experience,
+    Project,
+    Education,
+    SkillSet,
+    Certification,
+    Language,
+    CVResponse
+)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -23,60 +31,6 @@ app.add_middleware(
 
 if not os.path.exists('resumes'):
     os.makedirs('resumes')
-
-class Experience(BaseModel):
-    job_title: str = None
-    company_name: str = None
-    dateStart: str = None
-    dateEnd: str = None
-    nbrMonths: int = None
-    missions: str = None
-    technologies: List[str] = []
-    total_experience: Dict[str, int] = {}
-
-class Project(BaseModel):
-    project: str = None
-    description: str = None
-    technologies: List[str] = []
-
-class Education(BaseModel):
-    degree_type: str = None
-    degree_name: str = None
-    institution: str = None
-    dateStart: str = None
-    dateEnd: str = None
-
-class SkillSet(BaseModel):
-    hard_skills: List[str] = []
-    soft_skills: List[str] = []
-
-class Certification(BaseModel):
-    certification_name: str = None
-    institution: str = None
-    year_obtained: str = None
-
-class Language(BaseModel):
-    language: str = None
-    level: str = None
-
-class CVResponse(BaseModel):
-    firstName: str = None
-    lastName: str = None
-    current_position: str = None
-    civilite: str = None
-    phone_number: str = None
-    age: int = None
-    email: str = None
-    address: str = None
-    url: str = None
-    personalProfile: str = None
-    experience: List[Experience] = []
-    projects: List[Project] = []
-    education: List[Education] = []
-    skills: SkillSet = None
-    certifications: List[Certification] = []
-    languages: List[Language] = []
-    interests: List[str] = []
 
 @app.post("/upload/", response_model=CVResponse)
 async def upload_cv(file: UploadFile = File(...)):
